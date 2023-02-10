0f78cfe21a70f7e4192a3688382ec8ead90d3cfb
DiegoWSL <diego.castroalves0@gmail.com>
2023-02-10 12:39:24 -0300

    Requisito 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -6,12 +6,16 @@ class Feedback extends React.Component {
   state = {};
 
   render() {
-    const { assertions } = this.props;
+    const { assertions, score } = this.props;
     const minAssertions = 3;
     return (
       <div>
         <h1>Feedback</h1>
-        <p data-testid="feedback-total-score">{assertions}</p>
+        <p>
+          Numero de respostas acertadas:
+          <span data-testid="feedback-total-question">{assertions}</span>
+        </p>
+        <p data-testid="feedback-total-score">{score}</p>
         <p data-testid="feedback-text">
           {assertions < minAssertions ? 'Could be better...' : 'Well Done!'}
         </p>
@@ -22,10 +26,12 @@ class Feedback extends React.Component {
 
 Feedback.propTypes = {
   assertions: PropTypes.number,
+  score: PropTypes.number,
 }.isRequired;
 
 const mapStateToProps = ({ player }) => ({
   assertions: player.assertions,
+  score: player.score,
 });
 
 export default connect(mapStateToProps)(Feedback);
