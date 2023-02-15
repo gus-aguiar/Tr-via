eafc25738b0c02b7367b7d09974da65063b16a18
DiegoWSL <diego.castroalves0@gmail.com>
2023-02-15 14:56:15 -0300

    Style of Feedback Page

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,27 +1,44 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import { getGravatar } from '../helpers/apiTrivia';
+import styles from '../styles/Feedback.module.css';
 
 class Feedback extends React.Component {
   state = {
     picture: '',
+    message: '',
+    className: '',
   };
 
   componentDidMount() {
     this.getGravatarImage();
+    this.getMessage();
+  }
+
+  componentDidUpdate(prevProps) {
+    const { assertions } = this.props;
+    const minAssertions = 3;
+    if (assertions < minAssertions && prevProps.assertions >= minAssertions) {
+      this.setState({ message: 'Could be better...' });
+    }
   }
 
   handleRaking = () => {
-    const { name, score, email } = this.props;
+    const { name, score, email, history } = this.props;
     const ranking = JSON.parse(localStorage.getItem('ranking') || '[]');
     console.log(ranking);
     getGravatar(email).then((response) => {
       const newRanking = [...ranking, { name, score, picture: response }];
       localStorage.setItem('ranking', JSON.stringify(newRanking));
     });
+    history.push('/ranking');
+  };
+
+  handlePlayAgain = () => {
+    const { history } = this.props;
+    history.push('/');
   };
 
   getGravatarImage = () => {
@@ -31,34 +48,68 @@ class Feedback extends React.Component {
     });
   };
 
+  getMessage = () => {
+    const { assertions } = this.props;
+    const minAssertions = 3;
+
+    if (assertions < minAssertions) {
+      this.setState({
+        message: 'Could be better...',
+        className: styles.couldBetter,
+      });
+    } else {
+      this.setState({
+        message: 'Well Done!',
+        className: styles.wellDone,
+      });
+    }
+  };
+
   render() {
     const { name, assertions, score } = this.props;
-    const { picture } = this.state;
-    const minAssertions = 3;
+    const { picture, className, message } = this.state;
+
     return (
       <>
         <Header />
-        <div>
-          <h1>Feedback</h1>
-          <img src={ picture } alt={ name } />
-          <p data-testid="header-player-name">{name}</p>
-          <p>
-            Numero de respostas acertadas:
-            <span data-testid="feedback-total-question">{assertions}</span>
-          </p>
-          <p data-testid="feedback-total-score">{score}</p>
-          <p data-testid="feedback-text">
-            {assertions < minAssertions ? 'Could be better...' : 'Well Done!'}
-          </p>
-          <Link to="/">
-            <button data-testid="btn-play-again">Play Again</button>
-          </Link>
-
-          <Link to="/ranking">
-            <button data-testid="btn-ranking" onClick={ this.handleRaking }>
+        <div className={ styles.feeedbackContainer }>
+          <div className={ styles.feeedbackBackground }>
+            <img src={ picture } alt={ name } className={ styles.gravatarImage } />
+            <div className={ `${styles.nameContainer} ${className}` }>
+              <p data-testid="header-player-name">{name}</p>
+              <p data-testid="feedback-text">{message}</p>
+            </div>
+            <p className={ styles.scoreContainer }>
+              Você acertou
+              {' '}
+              <span data-testid="feedback-total-question">{assertions}</span>
+              {' '}
+              questões!
+            </p>
+            <p className={ styles.scoreContainer }>
+              Um total de
+              {' '}
+              <span data-testid="feedback-total-score">{score}</span>
+              {' '}
+              pontos!
+            </p>
+          </div>
+          <div className={ styles.btnContainer }>
+            <button
+              className={ styles.rankingButton }
+              data-testid="btn-ranking"
+              onClick={ this.handleRaking }
+            >
               Ranking
             </button>
-          </Link>
+            <button
+              className={ styles.playAgainButton }
+              data-testid="btn-play-again"
+              onClick={ this.handlePlayAgain }
+            >
+              Play Again
+            </button>
+          </div>
         </div>
       </>
     );
